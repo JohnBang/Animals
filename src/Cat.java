e6ba65498d4ac5ee118240de49dbe77275f0a1e9
= <=>
2016-11-08 01:30:34 +0200

    implemented class Cat

--- src/Cat.java
+++ src/Cat.java
@@ -1,7 +1,7 @@
 /**
  * Created by User on 22.10.2016.
  */
-public class Cat extends Animal {
+public class Cat extends Animal implements Animals {
     public Cat(String name, int koefSpeed){
         this.name = name;
         maxSpeed(koefSpeed);
