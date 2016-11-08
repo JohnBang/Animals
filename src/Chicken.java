15fdbf6f68d600a9188469a7253a36f19b065710
= <=>
2016-11-08 02:17:18 +0200

    Change Cat, Dog, Chicken, Animal

--- src/Chicken.java
+++ src/Chicken.java
@@ -8,6 +8,51 @@ public class Chicken extends Animal implements Animals {
         legs = 2;
         maxSpeed(koefSpeed);
     }
+    @Override
+    public void maxSpeed(int koef) {
+        this.speed = legs * koef;
+        if (maxSpeedValue < speed) maxSpeedValue = speed;
+    }
+    @Override
+    public void eat(int weight) {
+        this.weight += weight;
+    }
+
+    @Override
+    public void drink(int volume) {
+        this.volume +=  volume;
+    }
+
+    @Override
+    public void sleep(int time) {
+        this.time  += time;
+    }
+
+    @Override
+    public void printStates() {
+        System.out.println("\n For today " + name + ":\n\t - eat " + weight + " kg of Food,");
+        System.out.println("\t" + " - drink " + volume + " liters of water,");
+        System.out.println("\t" + " - sleep " + time + " hour.");
+        System.out.println("\t" + " - max speed equals " + speed);
+
+    }
+    @Override
+    public void printAllStates() {
+        System.out.println("\n For " + days + " days "  + name + ":\n\t - eat " + allWeight + " kg of Food,");
+        System.out.println("\t" + " - drink " + allVolume + " liters of water,");
+        System.out.println("\t" + " - sleep " + allTime + " hour,");
+        System.out.println("\t" + " - max speed equals " + maxSpeedValue);
+    }
+
+    @Override
+    public void nextDay() {
+        allWeight+=weight;
+        allVolume+=volume;
+        allTime+=time;
+
+        days++;
+        reset();
+    }
 
 }
 
